860b8ce13764ddf5e87dd04afc104e84ff78c623
Martin-Turtle <info@martinturtle.co.uk>
2023-01-06 23:02:53 +0000

    workbook1 answers

---
+++ src/SquareCalculator.java
@@ -0,0 +1,9 @@
+public class SquareCalculator {
+    public static void main(String[] args) {
+        double width = 13.5d;
+        double height = 25.3d;
+        System.out.println(width);
+        System.out.println(height);
+    }
+}
+
